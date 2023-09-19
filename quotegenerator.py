688aa7f902b9efc56db821e5b275710eb06a3aeb
Emily Song <es000091@sd38.bc.ca>
2023-09-19 11:04:41 -0700

    Added some new notes and Python files

---
+++ quotegenerator.py
@@ -0,0 +1,17 @@
+#Quote Generator
+#Author: Emily 
+#Date: 19 September 2023
+
+#Output a quote from someone famous
+print("That's one small step for a man, a giant leap for mankind. --neils armstrong ")
+
+#print out another quote
+print("The love of money is the root of all evil. --the bible")
+
+#print our another, different quote
+print("The only thing we have to fear is fear itself. --Franklin D. Roosevelt"end)
+
+#ask the user a question
+input("What's your favourite quote")
+
+#
