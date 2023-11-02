1648a36adb8050693a630ba09dee8b73db6d4c9f
Emily Song <es000091@sd38.bc.ca>
2023-11-02 15:02:17 -0700

    added changes to age prediction in 2049

---
+++ age_in_2049.py
@@ -0,0 +1,20 @@
+# Age in 2049
+# Author: Emily Song
+# 2 November 2023
+
+import time
+import random
+
+# Introduce the objectie
+print("Hi there, I am here to help you calculate your age by the year 2049! The answer will be based on the exact day at the time that you input your age. If your birthday has yet to occur then similarily the calculation will give you your age in 2049 without your birthday passing.")
+
+
+# Ask the user their age
+user_age = int(input("What is your age? "))
+
+user_age += 26
+
+print(f'Hi there, you will be {user_age} by the year 2049')
+
+
+
