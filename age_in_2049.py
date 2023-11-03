3303360db9ad1d0df5714730ab402795bb5760c6
Emily Song <es000091@sd38.bc.ca>
2023-11-03 11:51:06 -0700

    changed to accurate prompt

--- age_in_2049.py
+++ age_in_2049.py
@@ -1,21 +1,21 @@
-# World Travellor bot
+# Age in 2049
 # Author: Emily Song
 # 2 November 2023
 
 import time
 import random
 
-# Introduce the objectie to the user/ greet the user
-print("Hi there, I am a world travellor bot able to calculate your age by the year 2049! The answer will be based on the exact day at the time that you input your age. If your birthday has yet to occur then similarily the calculation will give you your age in 2049 without your birthday passing.")
+# Introduce the objective to the user/ greet the user
+print("Hi there, I am here to help you calculate your age by the year 2049! The answer will be based on the exact day at the time that you input your age. If your birthday has yet to occur then similarily the calculation will give you your age in 2049 without your birthday passing.")
 
 
 # Ask the user their age
-user_age = int(input("So, what is your age? "))
+user_age = int(input("What is your current age? "))
 
 user_age += 26
 
 # print out a response 
-print(f' Based on my years of expertise generating accurate predictions, You will be {user_age} by the year 2049')
+print(f' Based on my calculations, you will be {user_age} on the same month and date in year 2049')
 
 
 
