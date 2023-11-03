6008e865108b1602922f739127d80c5ee886d143
Emily Song <es000091@sd38.bc.ca>
2023-11-03 11:47:11 -0700

    added some changes to world travellor bot

---
+++ Notes/2 - Recommendation Systems.md
@@ -0,0 +1,30 @@
+Properties 
+#recommendation-systems #programming-level-1-2 #algorithms
+
+# Recommendation systems
+Popularity and "Likes"
+
+This type of paradigm is makes a suggestion based on how many people "like" the entity.
+
+One prime example of this, is the like button (heart) in Instagram. When you click on the heart button, Instagram records it somewhere along the other data like demographic and psychographic information. With this information, they might suggest to other users similar to you, products and services that you like. 
+
+We implemented the Popularity/Likes paradigm with our Favourite Bubble Tea activity. 
+
+## N-Point Rating system
+
+N-point rating systems are used by many services particularly Amazon.
+
+An example of an N-point rating system would be Amazon's product review system using stars.  
+	The system is rated using from 1-5 stars, with 5 stars being the highest and 1 star being the lowest. 
+
+This paradigm provides more granular information. Buyers are asked to rate the product uding s 5-point scale. 
+
+
+
+
+
+
+
+
+
+
--- age_in_2049.py
+++ age_in_2049.py
@@ -1,20 +1,21 @@
-# Age in 2049
+# World Travellor bot
 # Author: Emily Song
 # 2 November 2023
 
 import time
 import random
 
-# Introduce the objectie
-print("Hi there, I am here to help you calculate your age by the year 2049! The answer will be based on the exact day at the time that you input your age. If your birthday has yet to occur then similarily the calculation will give you your age in 2049 without your birthday passing.")
+# Introduce the objectie to the user/ greet the user
+print("Hi there, I am a world travellor bot able to calculate your age by the year 2049! The answer will be based on the exact day at the time that you input your age. If your birthday has yet to occur then similarily the calculation will give you your age in 2049 without your birthday passing.")
 
 
 # Ask the user their age
-user_age = int(input("What is your age? "))
+user_age = int(input("So, what is your age? "))
 
 user_age += 26
 
-print(f'Hi there, you will be {user_age} by the year 2049')
+# print out a response 
+print(f' Based on my years of expertise generating accurate predictions, You will be {user_age} by the year 2049')
 
 
 
