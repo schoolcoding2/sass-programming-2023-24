25bd49500499fbd215cf370787558b836693d5a9
Emily Song <es000091@sd38.bc.ca>
2023-11-06 11:54:34 -0800

    made changes to mcdoland's program

--- mcdolands.py
+++ mcdolands.py
@@ -6,25 +6,23 @@
 print("Hi there, welcome to McDoland's!")
 
 # Ask user if they would like a burger 
-user_burger = input("Would you like our signature $5 burgers?")
-
-user_burger = (input().strip(",.?!")) 
-
-
+user_burger = input("Would you like our signature $5 burgers?").lower().strip(",.?!")
 
 # Ask user if they would want fries for $3
 
-user_fries = input("Would you like our signature $3 fries?")
-
-user_fries = (input().strip(",.?!")) 
+user_fries = input("Would you like our signature $3 fries?").lower().strip(",.?!")
 
 # calculate total cost with 14% tax 
 
 if user_burger == "yes" and user_fries == "no":
-   print("Your total will be $5.70") 
+   print(f"Your total will be ${5.00*1.14}") 
 elif user_burger == "yes" and user_fries == "yes":
-   print(f"Your total is {8*1.14}")
+   print(f"Your total is ${8*1.14}")
+elif user_burger == "no" and user_fries == "yes":
+   print(f"Your total will be ${3.00*1.14}") 
 elif user_burger == "no" and user_fries == "no":
    print("okay, let me know if you ever decide to change your mind")
 else:
-    print("okay then")
+    print("okay then. The next server will be able to accommodate for your order. Thank you for your patience")
+
+   
