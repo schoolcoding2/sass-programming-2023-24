36b250af2229cafc424ec473e20cf31338d2402c
Emily Song <es000091@sd38.bc.ca>
2023-11-07 10:30:21 -0800

    added changes to olympic judging bot

--- olympic_judging.py
+++ olympic_judging.py
@@ -1,36 +1,20 @@
-# Semester Evaluator bot 
+
+# Olympic judging bot 
 # Author: Emily Song
-# 3 November 2023
+# 6 November 2023
 
 # Greet user 
-print("Hi sharks! I am a semester evaluator bot wanting to gauge your experience with your current semester courses. ")
+print("Hi user, I will be able to help you calculate your score from the corresponding judges. Please input the scores out of ten that you received from each of the five judges. Half points are allowed")
 
-# Ask user how many courses they are taking this semester
+# Ask the user to input the number they recieved from five corresponding judges.
 
-course_number = input("How many courses are you taking this semester. The course number can include online courses that you are taking this semester! ")
-print("okay, got it!")
+#set the beginning 
+judge_1 = float(input("Judge 1 score:"))
+judge_2 = float(input("Judge 2 score:"))
+judge_3 = float(input("Judge 3 score:"))
+judge_4 = float(input("Judge 4 score:"))
+judge_5 = float(input("Judge 5 score:"))
 
-course_number = 0
+average_score = (judge_1 + judge_2 + judge_3 + judge_4 + judge_5)/5
 
-for i in course_number:
-print(f'How do you rate the {course_number + 1} out of 5. 5 is the best with 1 being the worst.")
-
-
-total_rating = (input().strip(",.?!"))
-
-# Do some math regarding the result
-# Use the average score to represent the average rating of a single course
-average_rating_per_course= (total_rating / course_number)
-average_rating_final = int("{average_rating:.2f} / 5")
-
-
-# Present the results
-if average_rating_final == int(0-1):
-    print("Ouch")
-elif average_rating_final == int(1.0000001-3):
-    print("Not a bad semester")
-elif average_rating_final == int(3.00000001- 5):
-    print("Glad to hear that!")
-
-# The end
- print("Thank you for your feedback")
+print(f"your average score would be {average_score:.2f}/10")
