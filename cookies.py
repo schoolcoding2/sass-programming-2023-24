9a29d2bdb5e8f100f13e90f236381f72c268fbe8
Emily Song <es000091@sd38.bc.ca>
2023-11-23 11:17:42 -0800

    added changes to cookie bot

--- cookies.py
+++ cookies.py
@@ -2,34 +2,44 @@
 # Author: Emily Song 
 # 21 November 2023
 
+import random
 import turtle
 
 # Make baker turtle
 baker_turtle = turtle.Turtle()
 baker_turtle.color("brown")
 
-# Draw outline of cookie
-baker_turtle.penup()
-baker_turtle.goto(-5, -30)
-baker_turtle.pendown()
-baker_turtle.circle(30)
-baker_turtle.penup()
+ # applying the matehmatical concept of functions and inputs
+def make_cookie(x: int, y: int):
+    """Draws a cookie on the screen at (x,y)
+    Parameters:
+    x- x-coordinate of the centre of the cookie
+    y- y-coordinate of the centre of the cookie
+    """
 
-# Add chips
-baker_turtle.color("black")
+    # set up the outline of the cookie
+    baker_turtle.color("brown")
+    baker_turtle.penup()
+    baker_turtle.goto(-5 + x, -30 + y)
+    baker_turtle.pendown()
+    baker_turtle.circle(30)
+    baker_turtle.penup()
 
-baker_turtle.goto(0,0)
-baker_turtle.stamp()
 
-# Top right chip, bottom-right, bottom-left, middle) 
-baker_turtle.goto(10,10)
-baker_turtle.stamp()
-baker_turtle.goto(10,-10)
-baker_turtle.stamp()
-baker_turtle.goto(-10,-10)
-baker_turtle.stamp()
-baker_turtle.goto(-10,10)
-baker_turtle.stamp()
-turtle.done()
+    # center 
+    baker_turtle.goto(0 + x,0+ y )
+    baker_turtle.stamp()
 
-# can you move the cookie and the location of the chocolate chips 
+    # Top right chip, bottom-right, bottom-left, middle) 
+    baker_turtle.goto(10 + x,10 +y)
+    baker_turtle.stamp()
+    baker_turtle.goto(10 +x,-10 +y)
+    baker_turtle.stamp()
+    baker_turtle.goto(-10 +x,-10 +y)
+    baker_turtle.stamp()
+    baker_turtle.goto(-10+ x,10+y)
+    baker_turtle.stamp()
+    baker_turtle.penup()
+    
+make_cookie(100,100)
+make_cookie(0,0)
