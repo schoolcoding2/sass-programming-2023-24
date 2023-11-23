2fb15758ce2be196f4749ae8d90f619090658332
Emily Song <es000091@sd38.bc.ca>
2023-11-23 13:58:20 -0800

    added changes to data exercise

--- data_exercise.py
+++ data_exercise.py
@@ -45,115 +45,85 @@ print(f"there are {chicken_adobo} people who like chicken adobo as their favouri
 # Exercise 5: Find out how many people have the first letter of their first name start with "A".
 
 with open("./data_example.csv", encoding="utf-8") as f:
-    current_list = line.split(",")
-    current_a_names = 0
-    # string is just a list of characters 
+    count_a_names = 0 
+
     for line in f:
         current_list = line.lower().split(",")
-        current_list[0]
-        if current_list == "a":
-            current_a_names += 1
-    print("there are" + str(current_a_names) + "people who have a name starting with A")
+        current_name = current_list[0]
+        first_letter = current_name[0]
+
+        if first_letter == "a":
+            count_a_names += 1  
+
+    print(f"There are {count_a_names} people who have a name starting with 'A'")
         
 
 # Exercise 6: Find how many people come from Guangzhou
 
 with open("./data_example.csv", encoding="utf-8") as f:
-    current_list = line.split(",")  
-    # convert strings into list
-    guangzhou_counter = 1
+    guangzhou_count = 1  
+
     for line in f:
-        #find if we encounter the word "Guangzhou" 
+        current_list = line.split(",") 
+        # Check if "Guangzhou" is in the current list
         if "Guangzhou" in current_list:
-            guangzhou_counter += 1
-        # add one to the chicken adobo counter
-    print("there are" + str(guangzhou_counter) + "people who come from Guangzhou.")
+            guangzhou_count += 1 
+    print(f"There are {guangzhou_count} people who come from Guangzhou.")
+  
             
 # Exercise 7: Find how many people have a credit card number that is even. Hint: there are a couple of ways to solve this. You can either do this with the string or with the int.
 
+with open("./data_example.csv", encoding="utf-8") as f:
+    even_credit_card = 0 
+
+    for line in f:
+        current_list = line.lower().split(",")
+        card_number = current_list[3]
+        even_digit_credit_card = current_list[-1]
+
+    if even_digit_credit_card == "0":
+            even_credit_card += 1  
+    if even_digit_credit_card == "2":
+            even_credit_card += 1 
+    if even_digit_credit_card == "4":
+            even_credit_card += 1 
+    if even_digit_credit_card == "8":
+            even_credit_card += 1 
+print(f"There are {even_credit_card} people who have a credit card number that is even.")
+
 
 # Exercise 8: Design a way to find the most popular food. 
-with open("./data_example.csv", encoding="utf-8") as f:
-    current_list = line.split(",")
 
-    # list of all favourite food option submitted in the file 
-    favourite_food = [ "Hamburger", "Chicken Adobo", "Katsudon", "Fish and Chips", "Gyros", "Samosa", "Pad Thai", "Fish Tacos", "Cobb Salad", 
- "Kimchi", "Churros", "Pasta Carbonara", "Ramen", "Falafel", "Croissant", "Goulash", "Mole Poblano","Peking Duck","Sauerbraten",
-  "Rendan", "Poutine", "Pav Bhaji", "Tikka Masala", "Pizza", "Butter Chicken","Tandoori Chicken", "Jollof Rice", "Tiramisu",
-  "Couscous", "Pho", "Dim Sum", "Moussaka", "Lasagna", "Ceviche", "Croque Monsieur", "Pozole", "Sushi", "Chicken Satay"
-  "Biryani", "Pierogi", "Feijoada", "Kebab", "Tacos", "Paella", "Katsudon", "Sashimi", "Baklava"]
-    
-    Hamburger_counter = 0
-    Chicken_Adobo_counter = 0
-    Katsudon_counter = 0
-    Fish_and_Chips_counter = 0
-    Gyros_counter = 0
-    Samosa_counter = 0
-    Pad_thai_counter = 0
-    Fish_Tacos_counter = 0
-    Cobb_Salad_counter = 0
-    Kimchi_counter = 0
-    Churros_counter = 0
-    Pasta_Carbonara_counter = 0
-    Ramen_counter = 0
-    Falafel_counter = 0
-    Croissant_counter = 0
-    Goulash_counter = 0
-    Mole_Poblano_counter = 0
-    Peking_Duck_counter = 0
-    Sauerbraten_counter = 0
-    Rendan_counter = 0
-    Poutine_counter = 0
-    Pav_Bhaji_counter = 0
-    Tikka_Masala_counter = 0
-    Pizza_counter = 0
-    Butter_Chicken_counter = 0
-    Tandoori_Chicken_counter = 0
-    Jollof_Rice_counter = 0
-    Tiramisu_counter = 0
-    Couscous_counter = 0
-    Pho_counter = 0
-    Dim_Sum_counter = 0
-    Moussaka_counter = 0
-    Lasagna_counter = 0
-    Ceviche_counter = 0
-    Croque_Monsieur_counter = 0
-    Pozole_counter = 0
-    Sushi_counter = 0
-    Chicken_Satay_counter = 0
-    Biryani__counter = 0
-    Pierogi_counter = 0
-    Feijoada_counter = 0
-    Kebab_counter = 0
-    Tacos_counter = 0
-    Paella_counter = 0
-    Katsudon_counter = 0
-    Sashimi_counter = 0
-    Baklava_counter = 0
 
-    for line in f:
-        #find how many times each item is listed as favourite food
-        if "Hamburger" in current_list:
-            Hamburger_counter += 1
-    for line in f:
-        #find how many times each item is listed as favourite food
-        if "Chicken Adobo" in current_list:
-            Chicken_Adobo_counter += 1
-    for line in f:
-        #find how many times each item is listed as favourite food
-        if "Katsudon" in current_list:
-            Katsudon_counter += 1
-    for line in f:
-        #find how many times each item is listed as favourite food
-        if "Fish and Chips" in current_list:
-            Fish_and_Chips_counter += 1
+import csv
+from collections import Counter
 
-    for line in f:
-        #find how many times each item is listed as favourite food
-        if "Gyros" in current_list:
-             Gyros_counter += 1
-            
-    
+# Path to the file
+file_path = "./data_example.csv"
+
+# List of all favorite food options
+favorite_food = ["Hamburger", "Chicken Adobo", "Katsudon", "Fish and Chips", "Gyros", "Samosa", "Pad Thai", "Fish Tacos",
+                 "Cobb Salad", "Kimchi", "Churros", "Pasta Carbonara", "Ramen", "Falafel", "Croissant", "Goulash",
+                 "Mole Poblano", "Peking Duck", "Sauerbraten", "Rendang", "Poutine", "Pav Bhaji", "Tikka Masala",
+                 "Pizza", "Butter Chicken", "Tandoori Chicken", "Jollof Rice", "Tiramisu", "Couscous", "Pho",
+                 "Dim Sum", "Moussaka", "Lasagna", "Ceviche", "Croque Monsieur", "Pozole", "Sushi", "Chicken Satay",
+                 "Biryani", "Pierogi", "Feijoada", "Kebab", "Tacos", "Paella", "Katsudon", "Sashimi", "Baklava"]
+
+# Read the file and tally the occurrences of each food item
+food_counter = Counter()
+
+with open(file_path, encoding="utf-8") as f:
+    csv_reader = csv.reader(f)
+    for row in csv_reader:
+        for item in row:
+            if item.strip() in favorite_food:
+                food_counter[item.strip()] += 1
+
+# Find the most popular food item
+most_popular_food, occurrences = food_counter.most_common(1)[0]
+
+# Display the result
+print(f"The most popular food item is: {most_popular_food} with {occurrences} occurrences.")
   
 
 
