3e1111db2a9c5005f6d5e2bf437355a00bbbf78d
Emily Song <es000091@sd38.bc.ca>
2023-12-15 11:02:45 -0800

    made changes to image_example

--- Images_examples.py
+++ Images_examples.py
@@ -7,7 +7,8 @@
 # pillow = helps us open up the images 
 
 from PIL import Image
-import time
+
+
 def pixel_to_name(pixel: tuple) -> str:
     """Given a 3-tuple, return a string representing
     its colour
@@ -20,11 +21,9 @@ def pixel_to_name(pixel: tuple) -> str:
     """
     red, green, blue = pixel
 
-    if red < 25 and blue < 25 and green > 249:
+    if red < 140 and blue < 140 and green > 145:
         return "green"
-    else:
-        return "colour unknown"
-
+    
 # open up kid green
 with Image.open("./Images/kid-green.jpg") as im:
 
@@ -48,36 +47,29 @@ print(pixel)
 
 # December 14th, 2023 
 
-
-# open up kid green 
+# open up kid green
 with Image.open("./Images/kid-green.jpg") as im:
-    # create variables that store the width and height 
-    Image_height = im.height
-    Image_width = im.width
+    # create variables that store the width and height
+    image_height = im.height
+    image_width = im.width
 
     # open background image
     bg_im = Image.open("./Images/beach.jpg")
 
-    # *** REMEMBER TO CLOSE THE IMAGE WHEN DONE
-
-
     # starting at the top and working our way down
-    for y in range(Image_height):
-        # visit the pixels from the left to right
-        for x in range(Image_width):
-        # print this pixel's information 
-            pixel = im.getpixel((x,y))
-            
+    # visit the pixels from left to right
+    for y in range(image_height):
+        for x in range(image_width):
+            pixel = im.getpixel((x, y))
+
             if pixel_to_name(pixel) == "green":
-            # replace it with bg.pixel in same location 
-                bg_pixel = bg_im.getpixel((x,y))
+                # replace it with bg_im pixel in same location 
+                bg_pixel = bg_im.getpixel((x, y))
 
-                im.putpixel((x,y), bg_pixel)
+                im.putpixel((x, y), bg_pixel)
 
+ # REMEMBER TO CLOSE THE IMAGE WHEN DONE
+    bg_im.close()
 
-# *** REMEMBER TO CLOSE THE IMAGE WHEN DONE
-bg_im.close()
-
-# Save the image 
-im.save("./Images/output.jpg")
-
+    # Save image
+    im.save("./Images/output.jpg")
